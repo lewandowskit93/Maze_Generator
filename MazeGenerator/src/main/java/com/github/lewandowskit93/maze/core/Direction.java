311b5d856d3b2580b1724de186c352c01c8f6df5
Tomasz Lewandowski <lewandowski.tomasz93@yahoo.pl>
2014-05-30 20:53:44 +0200

    Direction extension
    1. Int -> Direction conversion
    2. Direction -> Int conversion
    3. EnumSet -> Int conversion

--- MazeGenerator/src/main/java/com/github/lewandowskit93/maze/core/Direction.java
+++ MazeGenerator/src/main/java/com/github/lewandowskit93/maze/core/Direction.java
@@ -1,5 +1,49 @@
 package com.github.lewandowskit93.maze.core;
 
+import java.util.EnumSet;
+
 public enum Direction {
 	NORTH,EAST,SOUTH,WEST;
+	
+	public static final EnumSet<Direction> getFromIntValue(int value) throws IllegalArgumentException{
+		validateRangeFor(value);
+		EnumSet<Direction> set = EnumSet.noneOf(Direction.class);
+		for(Direction direction : Direction.values()){
+			int mask = getIntValueOf(direction);
+			if((value & mask) == mask){
+				set.add(direction);
+			}
+		}
+		return set;
+	}
+	
+	private static final void validateRangeFor(int value) throws IllegalArgumentException{
+		int begin = getRangeBegin();
+		int end = getRangeEnd();
+		if(value < 0 || value > end){
+			throw new IllegalArgumentException(
+					"Illegal integer representation of direction values. "+
+					"Should belong to range <"+begin+","+end+">.");
+		}
+	}
+	
+	private static final int getRangeBegin(){
+		return 0;
+	}
+	
+	private static final int getRangeEnd(){
+		return (int)Math.pow(2, Direction.values().length)-1;
+	}
+
+	public static int getIntValueOf(Direction direction) {
+		return 1<<direction.ordinal();
+	}
+
+	public static int getIntValueOf(EnumSet<Direction> directions) {
+		int value = 0;
+		for(Direction direction : directions){
+			value+=getIntValueOf(direction);
+		}
+		return value;
+	}
 }
--- MazeGenerator/src/test/java/com/github/lewandowskit93/maze/core/DirectionTest.java
+++ MazeGenerator/src/test/java/com/github/lewandowskit93/maze/core/DirectionTest.java
@@ -1,6 +1,9 @@
 package com.github.lewandowskit93.maze.core;
 
 import static org.junit.Assert.*;
+
+import java.util.EnumSet;
+
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 
@@ -11,7 +14,7 @@ import org.junit.runner.RunWith;
 public class DirectionTest {
 
 	@Test
-	public void thereShouldBeFourDirections(){
+	public void thereShouldExactlyFourDirections(){
 		assertEquals(4,Direction.values().length);
 	}
 	
@@ -30,11 +33,78 @@ public class DirectionTest {
 		};
 	}
 	
-	
 	@Test
 	@Parameters(method = "getDirectionsWithItsOrdinals")
-	public void directionOrdinalShouldBeEqualToProvided(String direction,int value){
-		assertEquals(value,Direction.valueOf(direction).ordinal());
+	public void directionOrdinalShouldBeEqualToProvided(String direction,int ordinal){
+		assertEquals(ordinal,Direction.valueOf(direction).ordinal());
+	}
+	
+	private static final Object[] getValidDirectionCombinations(){
+		return new Object[]{
+				new Object[]{0,EnumSet.noneOf(Direction.class)},
+				new Object[]{1,EnumSet.of(Direction.NORTH)},
+				new Object[]{2,EnumSet.of(Direction.EAST)},
+				new Object[]{3,EnumSet.of(Direction.EAST,Direction.NORTH)},
+				new Object[]{4,EnumSet.of(Direction.SOUTH)},
+				new Object[]{5,EnumSet.of(Direction.SOUTH,Direction.NORTH)},
+				new Object[]{6,EnumSet.of(Direction.SOUTH,Direction.EAST)},
+				new Object[]{7,EnumSet.of(Direction.SOUTH,Direction.EAST,Direction.NORTH)},
+				new Object[]{8,EnumSet.of(Direction.WEST)},
+				new Object[]{9,EnumSet.of(Direction.WEST,Direction.NORTH)},
+				new Object[]{10,EnumSet.of(Direction.WEST,Direction.EAST)},
+				new Object[]{11,EnumSet.of(Direction.WEST,Direction.EAST,Direction.NORTH)},
+				new Object[]{12,EnumSet.of(Direction.WEST,Direction.SOUTH)},
+				new Object[]{13,EnumSet.of(Direction.WEST,Direction.SOUTH,Direction.NORTH)},
+				new Object[]{14,EnumSet.of(Direction.WEST,Direction.SOUTH,Direction.EAST)},
+				new Object[]{15,EnumSet.allOf(Direction.class)}
+		};
+	}
+	
+	@Test
+	@Parameters(method = "getValidDirectionCombinations")
+	public void shouldReturnDirectionsEnumSetRepresentationOfIntegerRepresentation(int i, EnumSet<Direction> representation){
+		assertEquals(representation,Direction.getFromIntValue(i));
+	}
+	
+	private static final Object[] getInvalidDirectionIntegerRepresentations(){
+		return new Object[]{-1,-2,-4,-8,-16,-17,-32,16,17,32};
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	@Parameters(method = "getInvalidDirectionIntegerRepresentations")
+	public void shouldBeUnableToGetDirectionsEnumSetRepresentationOfIntegerRepresentation(int value){
+		Direction.getFromIntValue(value);
+	}
+	
+	private static final Object[] getDirectionsWithValues(){
+		return new Object[]{
+				new Object[]{"NORTH", 1},
+				new Object[]{"EAST",2},
+				new Object[]{"SOUTH",4},
+				new Object[]{"WEST",8}
+		};
+	}
+	
+	@Test
+	@Parameters(method = "getDirectionsWithValues")
+	public void shouldReturnIntegerRepresentationOfDirection(Direction direction, int value){
+		assertEquals(value,Direction.getIntValueOf(direction));
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void shouldBeUnableToGetIntegerRepresentationOfDirection(){
+		Direction.getIntValueOf((Direction)null);
+	}
+	
+	@Test
+	@Parameters(method = "getValidDirectionCombinations")
+	public void shouldReturnIntegerRepresentationOfDirectionsEnumSet(int value, EnumSet<Direction> directions){
+		assertEquals(value,Direction.getIntValueOf(directions));
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void shouldBeUnableToGetIntegerRepresentationOfDirectionsEnumSet(){
+		Direction.getIntValueOf((EnumSet<Direction>)null);
 	}
 	
 }
