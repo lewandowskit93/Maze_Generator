3e3f0ca71f7486c977dc0fef3615e178c88bb192
Tomasz Lewandowski <lewandowski.tomasz93@yahoo.pl>
2014-06-01 05:49:05 +0200

    MazeDFSGenerator
    1. Can create generator of specified size
    2. Tests

--- MazeGenerator/src/main/java/com/github/lewandowskit93/maze/generators/MazeDFSGenerator.java
+++ MazeGenerator/src/main/java/com/github/lewandowskit93/maze/generators/MazeDFSGenerator.java
@@ -1,13 +1,32 @@
 package com.github.lewandowskit93.maze.generators;
 
+import com.github.lewandowskit93.maze.core.InvalidMazeHeightException;
+import com.github.lewandowskit93.maze.core.InvalidMazeWidthException;
 import com.github.lewandowskit93.maze.core.Maze;
 import com.github.lewandowskit93.maze.core.MazeGenerator;
 
 public class MazeDFSGenerator implements MazeGenerator {
+	
+	private int width,height;
+
+	public MazeDFSGenerator(int width, int height) {
+		if(width<=0)throw new InvalidMazeWidthException(width);
+		if(height<=0)throw new InvalidMazeHeightException(height);
+		this.width=width;
+		this.height=height;
+	}
 
 	@Override
 	public Maze generateMaze() {
 		return null;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
 	}
 
 }
--- MazeGenerator/src/test/java/com/github/lewandowskit93/maze/generators/MazeDFSGeneratorTest.java
+++ MazeGenerator/src/test/java/com/github/lewandowskit93/maze/generators/MazeDFSGeneratorTest.java
@@ -1,11 +1,52 @@
 package com.github.lewandowskit93.maze.generators;
 
 import static org.junit.Assert.*;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.github.lewandowskit93.maze.core.InvalidMazeSizeException;
+
+@RunWith(JUnitParamsRunner.class)
 public class MazeDFSGeneratorTest {
-
 	
+	@SuppressWarnings("unused")
+	private static final Object[] getValidSizes(){
+		return new Object[]{
+			new Object[]{1,1},
+			new Object[]{1,3},
+			new Object[]{3,1},
+			new Object[]{16,16}
+		};
+	}
+	
+	@Test
+	@Parameters(method = "getValidSizes")
+	public void shouldBeAbleToCreateGeneratorOfSpecifiedSize(int width, int height){
+		MazeDFSGenerator generator = new MazeDFSGenerator(width,height);
+		assertEquals(width,generator.getWidth());
+		assertEquals(height,generator.getHeight());
+	}
+	
+	@SuppressWarnings("unused")
+	private static final Object[] getInvalidSizes(){
+		return new Object[]{
+			new Object[]{-1,1},
+			new Object[]{1,-3},
+			new Object[]{0,1},
+			new Object[]{16,0},
+			new Object[]{0,0},
+			new Object[]{-16,-16}
+		};
+	}
 
+	@Test(expected = InvalidMazeSizeException.class)
+	@Parameters(method = "getInvalidSizes")
+	public void shouldBeUnableToCreateGeneratorOfSpecifiedSize(int width, int height){
+		MazeDFSGenerator generator = new MazeDFSGenerator(width,height);
+		assertEquals(width,generator.getWidth());
+		assertEquals(height,generator.getHeight());
+	}
 }
